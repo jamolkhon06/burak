34e0fcfadc920fe24ae1df3fa2d681bdfe9e5e9d
Joseph <jamolxonsiddikovich@gmail.com>
2026-09-20 13:04:31 +0900

    BRR: initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("Executed");
+
+import moment from "moment";
+
+const currentTime = moment().format('YYYY MM DD');
+console.log(currentTime)
+
+const person: string = "Joseph";
+const count: number = 100;
