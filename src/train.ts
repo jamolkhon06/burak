e34c468a7a7158d72f5d7798218b93c931ee118e
Joseph <jamolxonsiddikovich@gmail.com>
2026-09-23 23:36:06 +0900

    completed task-M

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+// Task-M
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin. MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+function getSquareNumbers(arr: number[]) {
+    let newArr: string[] = [];
+
+    arr.forEach(num => {
+        newArr.push(`{number: ${num}, square: ${num * num}`);
+    })
+
+    return newArr
+}
+
+const result = getSquareNumbers([1, 2, 3])
+console.log(result)
