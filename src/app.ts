7fab61d741a854181c671e520a5777b0e0f7de8a
Joseph <jamolxonsiddikovich@gmail.com>
2026-09-24 23:01:42 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import router from './router';
+import routerAdmin from './routerAdmin'
 
 // 1. Entrance
 const app = express();
@@ -15,6 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // 4. Routers
-app.use('/', router) // Middleware Design Pattern
+// BSSR: ejs
+app.use('/admin', routerAdmin) // EJS
+app.use('/', router) // REACT
 
 export default app;
