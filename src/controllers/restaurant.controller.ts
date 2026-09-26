df2b0f7b8fe0a043ce6902f3081012ff465c63e9
Joseph <jamolxonsiddikovich@gmail.com>
2026-09-27 00:53:25 +0900

    feat: create project standarts

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,6 +5,7 @@ import MemberService from '../models/Member.service';
 const restaurantController: T = {}
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
+        console.log('Home page')
         res.send('Home Page')
     } catch(err) {
         console.error('Error, goHome', err);
@@ -13,6 +14,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
+        console.log('Login page')
         res.send('Login Page')
     } catch(err) {
         console.error('Error, getLogin', err);
@@ -21,6 +23,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
+        console.log('Signup Page')
         res.send('Signup Page')
     } catch(err) {
         console.error('Error, getSignup', err);
--- src/train.ts
+++ src/train.ts
@@ -1,12 +1,22 @@
+/* Project Standarts
+    ~ Logging standarts
+    ~ Naming standarts
+        function, method, variable => CamelCase
+        class => PascalCase
+        folder => KebabCase
+        css classes => SnakeCase
+    ~ Error handling
+    ~ 
+*/
+
 // Task-N
 // Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin. MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
-function palindromCheck(str: string): boolean {
+/* function palindromCheck(str: string): boolean {
     const reversedStr = str.split('').reverse().join('');
     return str === reversedStr;
 }
 const result = palindromCheck("dad");
-console.log(result);
-
+console.log(result); */
 
 // Task-M
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin. MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
